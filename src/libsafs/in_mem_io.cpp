2fad1896412e053925fa9f64d33a374346f74f22
Da Zheng <zhengda1936@gmail.com>
2016-11-19 15:10:54 -0500

    Update SAFS.

--- src/libsafs/in_mem_io.cpp
+++ src/libsafs/in_mem_io.cpp
@@ -17,6 +17,10 @@
  * limitations under the License.
  */
 
+#ifdef USE_NUMA
+#include <numa.h>
+#endif
+
 #include <boost/format.hpp>
 
 #include "in_mem_io.h"
@@ -39,7 +43,11 @@ public:
 	}
 
 	void operator()(char *buf) const {
+#ifdef USE_NUMA
 		numa_free(buf, size);
+#else
+		free(buf);
+#endif
 	}
 };
 
@@ -101,9 +109,15 @@ NUMA_buffer::NUMA_buffer(size_t length,
 	// Allocate memory for each NUMA node.
 	for (size_t i = 0; i < bufs.size(); i++) {
 		if (buf_lens[i] > 0) {
+#ifdef USE_NUMA
 			bufs[i] = std::shared_ptr<char>(
 					(char *) numa_alloc_onnode(buf_lens[i], i),
 					numa_delete(buf_lens[i]));
+#else
+			bufs[i] = std::shared_ptr<char>(
+					(char *) malloc_aligned(buf_lens[i], PAGE_SIZE),
+					numa_delete(buf_lens[i]));
+#endif
 			assert(bufs[i]);
 		}
 	}
@@ -285,8 +299,6 @@ NUMA_buffer::ptr NUMA_buffer::load_safs(const std::string &file_name,
 				+ file_name);
 	const size_t MAX_IO_SIZE = std::min(256UL * 1024 * 1024,
 			mapper.get_range_size());
-	// TODO maybe I should create a thread for each NUMA node to load data.
-	// so the memory is allocated in each NUMA node.
 	for (off_t off = 0; (size_t) off < file_size; ) {
 		data_loc_t loc(io_factory->get_file_id(), off);
 		size_t load_size = std::min(MAX_IO_SIZE, file_size - off);
