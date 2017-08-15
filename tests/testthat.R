14ecd70e524222b40f08619eef693eed4e2ee32d
Da Zheng <zhengda1936@gmail.com>
2017-08-14 21:06:46 -0400

    Update tests in travis.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(FlashR)
 
-ret <- test_file("Rpkg/inst/tests/test_dense.R")
+ret <- test_file("inst/tests/test_dense.R")
 msgs <- capture.output(ret)
 tests <- length(msgs) / 3
 fails <- sum(as.integer(sapply(strsplit(msgs[(tests+2):(2*tests)], " +"),
